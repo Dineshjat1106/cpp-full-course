6245865221d07527caf88c6d7e634f9d77022e03
Dinesh Kalera <dj5510372@gmail.com>
2025-05-16 07:53:02 +0530

    pattern-15.cpp

--- pattern-printing-8/pattern-15.cpp
+++ pattern-printing-8/pattern-15.cpp
@@ -4,7 +4,7 @@ int main(){
     for(int i=1;i<=5;i++){
         for(int j=1;j<=5;j++){
             if(j==3 || i==3) cout<<"* ";
-            else cout<<"  ";  
+            else cout<<" ";  
         }
         cout<<endl;
     }
